be86f496ddad1c8dbd803bb9be308ea380afdf71
manglij <nicolas.lepage41@gmail.com>
2025-01-07 15:15:49 +0100

    premiere partie de la connexion

---
+++ src/Classes/Views/connexion.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="/static/css/connexion.css">
+    <title>Quizz</title>
+</head>
+
+<body>
+    <div class="header">
+        <h1>Connexion a notre quizz</h1>
+    </div>
+    <div class="content">
+
+        <form class="connexion" method="POST" action="tentative_co.php">
+            <p>CONNEXION</p>
+            <input type="text" name="nom" id="nom" placeholder="Nom" />
+            <input type="password" name="passwd" id="passwd" placeholder="Mot de passe" />
+            <button type="button" name="toggle-password" id="toggle-password" class="toggle-password" data-target="passwd">afficher</button>
+            <?php session_start();
+            if (!empty($_SESSION['connexion_fail'])) {
+                $_SESSION['connexion_fail'] = false;
+                echo "<p class='fail'>L'identifiant ou le mot de passe est incorrect</p>";
+            } ?>
+            <input type="submit" value="Se connecter" />
+        </form>
+    </div>
+    
+    <script>
+    document.querySelectorAll('.toggle-password').forEach(button => {
+        button.addEventListener('click', () => {
+            const targetId = button.getAttribute('data-target');
+            const passwordField = document.getElementById(targetId);
+            if (passwordField.type === 'password') {
+                passwordField.type = 'text';
+                button.textContent = 'Cacher';
+            } else {
+                passwordField.type = 'password';
+                button.textContent = 'Afficher';
+            }
+        });
+    });
+    </script>
+</body>
+</html>
--- src/Data/script.sql
+++ src/Data/script.sql
@@ -9,6 +9,22 @@ create table form (
     updated_at timestamp default current_timestamp
 );
 
+
+CREATE TABLE UTILISATEUR(
+    nomU VARCHAR(5) PRIMARY KEY,
+    mdp VARCHAR(100),
+    typeU VARCHAR(3)
+);
+
+CREATE TABLE JOUEURS(
+    idJ INT PRIMARY KEY,
+    nomU VARCHAR(50),
+    scoreJ INT DEFAULT 0,
+    FOREIGN KEY(nomU) REFERENCES UTILISATEUR(nomU)
+);
+
+
+
 insert into form (id, name, description) values (1, 'Form 1', 'Description 1'),
                         (2, 'Form 2', 'Description 2'),
                         (3, 'Form 3', 'Description 3'), 
