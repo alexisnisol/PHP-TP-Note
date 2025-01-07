be63bfa69f7ae2ac672079669a0dd66d5da0f7e2
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2025-01-07 15:13:17 +0100

    Correction de la base

--- src/Classes/Provider/DataLoaderJson.php
+++ src/Classes/Provider/DataLoaderJson.php
@@ -5,7 +5,7 @@ namespace Provider;
 
 final class DataLoaderJson extends DataLoader {
     
-    public function _construct(string $filename) {
-        $this->content = json_decode(file_get_contents($filename), true);
+    public function __construct(string $path) {
+        $this->content = json_decode(file_get_contents($path), true);
     }
 }
