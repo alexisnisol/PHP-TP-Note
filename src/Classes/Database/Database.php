1da11d5863e6609d719e1f2b2930b1977e7a8ae8
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2025-01-09 10:55:55 +0100

    Implémentation de la gestion de base de données (abstraction, support mysql, sqlite, modulable)

---
+++ src/Classes/Database/Database.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Classes\Database;
+
+use PDO;
+use PDOException;
+
+abstract class Database implements IDatabase
+{
+
+    public function query($statement) {
+        $pdo = $this->getPDO();
+        return $pdo->query($statement);
+    }
+
+    public function execute($statement) {
+        $pdo = $this->getPDO();
+        return $pdo->exec($statement);
+    }
+
+    public function prepare($statement, $options = []) {
+        $pdo = $this->getPDO();
+        return $pdo->prepare($statement, $options);
+    }
+
+    public function loadContents() {
+        if (!$this->databaseExists()) {
+            $this->createDatabase();
+        }
+    }
+
+    public function createDatabase() {
+        $this->execute(file_get_contents(ROOT . '/Data/creaQuiz.sql'));
+        //$this->execute(file_get_contents(ROOT . '/Data/insQuiz.sql'));
+    }
+}
