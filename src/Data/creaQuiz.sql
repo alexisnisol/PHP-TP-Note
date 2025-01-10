4d82e802c9082a246d38172774935c4588dac25a
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2025-01-10 10:03:09 +0100

    Correction bd

--- src/Data/creaQuiz.sql
+++ src/Data/creaQuiz.sql
@@ -5,40 +5,40 @@ DROP TABLE IF EXISTS UTILISATEUR;
 
 CREATE TABLE QUIZ
 (
-    PRIMARY KEY (id_Quiz),
     id_Quiz INT NOT NULL,
     name_Q  VARCHAR(42),
-    theme   VARCHAR(42)
+    theme   VARCHAR(42),
+    PRIMARY KEY (id_Quiz)
 );
 
 CREATE TABLE QUESTION
 (
-    PRIMARY KEY (id_Q, id_Quiz),
     id_Q    INT NOT NULL,
     id_Quiz INT NOT NULL,
     type_Q  VARCHAR(42),
     label   VARCHAR(42),
     choices VARCHAR(42),
     correct VARCHAR(42),
+    PRIMARY KEY (id_Q, id_Quiz),
     WITH (id_Quiz) REFERENCES QUIZ (id_Quiz)
 );
 
 CREATE TABLE UTILISATEUR
 (
-    PRIMARY KEY (uuid),
     uuid   INT NOT NULL,
     nom_U  VARCHAR(42),
     mdp    VARCHAR(42),
-    type_U VARCHAR(42)
+    type_U VARCHAR(42),
+    PRIMARY KEY (uuid)
 );
 
 CREATE TABLE PARTICIPE
 (
-    PRIMARY KEY (id_Quiz, uuid, date),
     id_Quiz INT NOT NULL,
     uuid    INT NOT NULL,
     score   FLOAT,
     date    DATE DEFAULT (CURRENT_DATE),
-    WITH (id_Quiz) REFERENCES QUIZ (id_Quiz),
-    WITH (uuid) REFERENCES UTILISATEUR (uuid)
+    PRIMARY KEY (id_Quiz, uuid, date),
+    FOREIGN KEY (id_Quiz) REFERENCES QUIZ (id_Quiz),
+    FOREIGN KEY (uuid) REFERENCES UTILISATEUR (uuid)
 );
