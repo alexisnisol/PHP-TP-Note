83c452bee6d2289916a0e541107d02fc99e2221a
manglij <nicolas.lepage41@gmail.com>
2025-01-10 15:30:45 +0100

    correction review

--- src/Classes/Controllers/Auth/Auth.php
+++ src/Classes/Controllers/Auth/Auth.php
@@ -14,17 +14,10 @@ class Auth
     static function getCurrentUser() {
         if (self::isUserLoggedIn()) {
             return [
-                'id' => $_SESSION['user_id'],
-                'name' => $_SESSION['user_name'],
-                'email' => $_SESSION['user_email']
+                'id' => $_SESSION['uuId'],
+                'nom' => $_SESSION['nom'],
+                'score' => $_SESSION['score']
             ];
-        }
-        return null;
-    }
-
-    static function getCurrentUserObj() {
-        if (self::isUserLoggedIn()) {
-            return self::getUserById($_SESSION['user_id']);
         }
         return null;
     }
@@ -35,23 +28,6 @@ class Auth
         }
     }
 
-    static function getUserById($id) {
-        $query = App::getApp()->getDB()->prepare('SELECT * FROM PERSONNE WHERE id_p = :id_p');
-        $query->execute(array(':id_p' => $id));
-        $user = $query->fetch();
-
-        if($user){
-            if (isset($user['salaire'])) {
-                $userObj = new Instructor($user['id_p'], $user['nom'], $user['prenom'], $user['adresse'], $user['email'], $user['telephone'], $user['niveau'], $user['poids'], $user['mdp'], $user['salaire'], $user['experience']);
-            } else {
-                $userObj = new User($user['id_p'], $user['nom'], $user['prenom'], $user['adresse'], $user['email'], $user['telephone'], $user['niveau'], $user['poids'], $user['mdp']);
-            }
-        } else {
-            $userObj = null;
-        }
-
-        return $userObj;
-    }
 
     static function getUserByName($nom) {
         $query = App::getApp()->getBD()->prepare('SELECT * FROM UTILISATEUR WHERE nom_U = :nom');
