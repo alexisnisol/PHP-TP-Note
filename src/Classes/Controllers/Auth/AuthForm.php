84f14f3d904507cf91a2db2c5371ccf178bb2add
manglij <nicolas.lepage41@gmail.com>
2025-01-14 14:24:42 +0100

    inscription

--- src/Classes/Controllers/Auth/AuthForm.php
+++ src/Classes/Controllers/Auth/AuthForm.php
@@ -1,23 +1,29 @@
 <?php
 
 namespace Classes\Controllers\Auth;
+use App;
 
 class AuthForm {
 
     static function checkLoginForm($nom, $password) {
         $user = Auth::getUserByName($nom);
+        $error = '<br>';
 
         if($user){
             //verify password
             if(password_verify($password, $user['mdp'])){
+                error_log('User connected : ' . $user['nom']);
                 //TODO : store user in session instead of user_id (SESSION['user']['id'])
                 $_SESSION['nom'] = $user['nom'];
                 $_SESSION['type'] = $user['type'];
                 if ($user['type'] == 'ADM') {
-                    header('Location: /admin');}
+                    error_log('Admin connected');
+                    header('index.php?action=admin');}
                 elseif ($user['type'] == 'USER') {
-                    header('Location: /ListeQuiz');
+                    error_log('User connected');
+                    header('Location: index.php?action=listeQuiz');
                 } else {
+                    error_log('nobody connected');
                 header('Location: /');
             }}else{
                 $error = 'Mot de passe incorrect';
@@ -29,16 +35,22 @@ class AuthForm {
         return $error;
     }
 
-    static function checkRegisterForm($email, $password, $firstName, $lastName, $address, $phone, $level, $weight) {
-        $user = Auth::getUserByEmail($email);
+    static function checkRegisterForm($nom, $password, $password2, $type) {
+        $user = Auth::getUserByName($nom);
 
-        if(!$user){
-            $_SESSION['nom'] = $user['nom'];
-            $_SESSION['type'] = $user['type'];
-            header('Location: /');
+
+
+        if(is_null($user)&&$password == $password2){
+
+            $query = App::getApp()->getBD()->prepare('INSERT INTO UTILISATEUR (uuid,nom_U, mdp, type_U) VALUES (:uuid, :nom, :mdp, :typeU)');
+            $query->execute(array(':nom' => $nom, ':mdp' => password_hash($password, PASSWORD_DEFAULT), ':typeU' => $type, ':uuid' => uniqid()));
+            header('Location: index.php?action=connexion');
+
         }else{
-            $error = "Un utilisateur avec cet email existe déjà";
+            $error = "Un utilisateur avec ce nom existe déjà";
         }
+        if($password != $password2){
+            $error = "Les mots de passe ne correspondent pas";}
 
         return $error;
     }
--- src/templates/auth/inscription.php
+++ src/templates/auth/inscription.php
@@ -7,19 +7,26 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     //get post data
     $nom = $_POST['nom'];
     $password = $_POST['passwd'];
-    $error = AuthForm::checkLoginForm($nom, $password);
+    $password2 = $_POST['passwd2'];
+    $type = 'USER';
+    $error = AuthForm::checkRegisterForm($nom, $password, $password2, $type);
 }
 ?>
+    <div class="header">
+        <h1>inscription a notre quizz</h1>
+    </div>
     <div class="content">
 
         <form class="connexion" method="POST" action="#">
-            <p>CONNEXION</p>
+            <p>Inscription</p>
             <input type="text" name="nom" id="nom" placeholder="Nom" />
             <input type="password" name="passwd" id="passwd" placeholder="Mot de passe" />
             <button type="button" name="toggle-password" id="toggle-password" class="toggle-password" data-target="passwd">afficher</button>
+            <input type="password" name="passwd2" id="passwd2" placeholder="Confirmer votre Mot de passe" />
+            <button type="button" name="toggle-password" id="toggle-password" class="toggle-password" data-target="passwd2">afficher</button>
         <?php 
         if (isset($error)) {
-                echo "<p class='fail'>L'identifiant ou le mot de passe est incorrect</p>";
+                echo "<p class='fail'>".$error."</p>";
             } ?>
             <input type="submit" value="Se connecter" />
         </form>
