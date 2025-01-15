9144de4dcea5aceb6eb57f4827b30cbaab0f9f5b
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2025-01-15 01:49:15 +0100

    Supression fichiers inutiles

--- src/Action/form.php
+++
@@ -1,8 +0,0 @@
-<h1>Remplissez le formulaire</h1>
-<form action="" method="post">
-    <?php
-    foreach($questions as $question):?>
-    <p><?php echo $question->render()?></p>
-    <?php endforeach;?>
-    <input type="submit" value="valider">
-</form>
