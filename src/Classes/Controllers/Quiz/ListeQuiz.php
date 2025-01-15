28fea747238e8dc987f8e76e953314797dbe647a
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2025-01-15 20:32:55 +0100

    Fix requete

--- src/Classes/Controllers/Quiz/ListeQuiz.php
+++ src/Classes/Controllers/Quiz/ListeQuiz.php
@@ -6,40 +6,45 @@ use App;
 
 class ListeQuiz
 {
-
-    static function getAllPlayerQuiz($uuid)
+    static function getLastParticipation($idQuiz, $idPlayer)
     {
         $query = App::getApp()->getBD()->prepare("
         SELECT
-            QUIZ.id_Quiz,
-            QUIZ.name_Q,
-            QUIZ.theme,
-            PARTICIPE.score
-        FROM QUIZ
-        LEFT JOIN PARTICIPE ON QUIZ.id_Quiz = PARTICIPE.id_Quiz AND PARTICIPE.uuid = :uuid
-        ORDER BY QUIZ.id_Quiz
+            *
+        FROM PARTICIPE
+        WHERE id_Quiz = :idQuiz AND uuid = :idPlayer
+        ORDER BY date DESC
         ");
-        $query->execute([':uuid' => $uuid]);
-        $quiz = $query->fetchAll();
-        return $quiz;
+        $query->execute([':idQuiz' => $idQuiz, ':idPlayer' => $idPlayer]);
+        return $query->fetch();
     }
-
 
     static function getAllPlayerQuizWithTotalQuestions($uuid)
     {
         $query = App::getApp()->getBD()->prepare("
-        SELECT
-            QUIZ.id_Quiz,
-            QUIZ.name_Q,
-            QUIZ.theme,
-            PARTICIPE.score,
-            COUNT(QUESTION.id_Q) as totalQ
-        FROM QUIZ
-        LEFT JOIN PARTICIPE ON QUIZ.id_Quiz = PARTICIPE.id_Quiz AND PARTICIPE.uuid = :uuid
-        LEFT JOIN QUESTION ON QUIZ.id_Quiz = QUESTION.id_Quiz
-        GROUP BY QUIZ.id_Quiz
-        ORDER BY QUIZ.id_Quiz
-        ");
+    SELECT
+        QUIZ.id_Quiz,
+        QUIZ.name_Q,
+        QUIZ.theme,
+        LAST_PARTICIPATION.score,
+        COUNT(QUESTION.id_Q) as totalQ
+    FROM QUIZ
+    LEFT JOIN (
+        SELECT 
+            PARTICIPE.id_Quiz,
+            PARTICIPE.score
+        FROM PARTICIPE
+        WHERE PARTICIPE.uuid = :uuid
+          AND PARTICIPE.date = (
+              SELECT MAX(subP.date)
+              FROM PARTICIPE subP
+              WHERE subP.uuid = :uuid AND subP.id_Quiz = PARTICIPE.id_Quiz
+          )
+    ) AS LAST_PARTICIPATION ON QUIZ.id_Quiz = LAST_PARTICIPATION.id_Quiz
+    LEFT JOIN QUESTION ON QUIZ.id_Quiz = QUESTION.id_Quiz
+    GROUP BY QUIZ.id_Quiz
+");
+
         $query->execute([':uuid' => $uuid]);
         $quiz = $query->fetchAll();
         return $quiz;
